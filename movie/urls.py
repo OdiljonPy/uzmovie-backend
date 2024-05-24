eb451512cd3ece6b3cd124d2f432e85b73c586a1
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-05-24 21:20:20 +0500

    urls.py created and added necessary paths: get-one, get-all, by-genre, by- actor and ext.

---
+++ movie/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+# from .views import MovieViewSet, CommentViewSet, SavedViewSet
+
+urlpatterns = [
+    # MOVIE
+    # path('get-all/', MovieViewSet.as_view({'get': 'get_all'})),
+    # path('get-one/', MovieViewSet.as_view({'get': 'get_one'})),
+    # path('by-genre/', MovieViewSet.as_view({'get': 'get_by_genre'})),
+    # path('by-director/', MovieViewSet.as_view({'get': 'get_by_director'})),
+    # path('by-actor/', MovieViewSet.as_view({'get': 'get_by_actor'})),
+    # # SAVED
+    # path('save/', SavedViewSet.as_view({'post': 'save_movie'})),
+    # # COMMENT
+    # path('comment/create/', CommentViewSet.as_view({'post': 'create'})),
+    # path('comment/delete/', CommentViewSet.as_view({'delete': 'delete'})),
+
+
+    # path('comment/update/', CommentViewSet.as_view({'patch': 'update'})),
+
+]
