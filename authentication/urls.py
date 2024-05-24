8d4bf42eab5122fcc6e7f1445d054d5d05d43682
bilol <bilolnorov7@gmail.com>
2024-05-24 20:46:08 +0500

    Authentication: serializers and utils files created and 3 to-do urls added

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
+    # path('register/'),
+    # path('reset-password/'),
+    # path('reset-password-verify/'),
 ]
