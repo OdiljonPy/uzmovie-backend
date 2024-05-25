5133afb63d2cd1131269a74f45f6ef627b8c1237
newmaster1047 <jaloliddinmine00@gmail.com>
2024-05-25 19:06:07 +0500

    generate_otp_code, function: watch_move(user_id, move_id) # checking premium status

---
+++ payments/utils.py
@@ -0,0 +1,23 @@
+from random import randint
+from authentication.models import User
+from movie.models import Movie
+from .models import Subscription, Status
+
+
+def generate_otp_code():
+    return randint(10000, 99999)
+
+
+def watch_movie(user_id, movie_id):
+    user = User.objects.filter(id=user_id).first()
+    movie = Movie.objects.filter(id=movie_id).first()
+    subscription = Subscription.objects.filter(user=user)
+    status = Status.objects.filter(name='active')
+
+    if movie.premium_status is False:
+        return True
+
+    elif subscription.status == status:
+        return True
+
+    return False
