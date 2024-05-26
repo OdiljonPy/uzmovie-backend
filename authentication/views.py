f3886691b2cd08a8b81bb93da7417fccc4c7f007
bilol <bilolnorov7@gmail.com>
2024-05-26 23:23:15 +0500

    Authentication: in views demo viewsets added

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class AuthenticateViewSet(ViewSet):
+    # def register(self, request, *args, **kwargs):
+    #     username = request.data.get('username')
+    #     password = request.data.get('password')
+    #     serializer = UserSerializer(data={'username': username, 'password': password})
+    #     if serializer.is_valid():
+    #         serializer.validated_data['is_verified'] = False
+    #         serializer.save()
+    #         return Response(data=)
+
+    def verify_register(self, request, *args, **kwargs):
+        pass
+
+    def login(self, request, *args, **kwargs):
+        pass
+
+
+class ResendAndResetViewSet(ViewSet):
+    def reset_password(self, request, *args, **kwargs):
+        pass
+
+    def verify_reset_password(self, request, *args, **kwargs):
+        pass
+
+    def resend_otp_code(self, request, *args, **kwargs):
+        pass
+
