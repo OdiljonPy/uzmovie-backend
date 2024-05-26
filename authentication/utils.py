7403342661d1d3894be1b7123c6519a11b3df085
bilol <bilolnorov7@gmail.com>
2024-05-26 23:19:37 +0500

    Authentication: in utils username validation added

--- authentication/utils.py
+++ authentication/utils.py
@@ -7,7 +7,7 @@ BOT_ID = "6725176067:AAFYwaMgrBHuvq8V-iwzLOLNRjIVH1UYIBU"
 CHAT_ID = "" # TElegram guruh chat id kerak
 TELEGRAMBOT_URL = "https://api.telegram.org/bot{}/sendMessage?text={}&chat_id={}"
 
-
+number_codes = ('99', '98', '97', '95', '94', '93', '91', '90', '77', '55', '33', '71')
 def send_otp_code_telegram(otp_obj):
     message = (f"Project: UzMovie\n PhoneNumber: {otp_obj.user}\n "
                f"code: {otp_obj.otp_code}\n key: {otp_obj.otp_key}\n "
@@ -25,6 +25,14 @@ def check_code_expire(created_at):
     if current_time - created_at > allowed_minut:
         return False
     return True
+
+
+def username_validation(username):
+    if len(username) == 12:
+        if username[:3] == '998':
+            if username[3:5] in number_codes:
+                return True
+    return False
 
 
 
