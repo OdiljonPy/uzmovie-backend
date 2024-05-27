e6aefc883368b227a1912dd889cc9efa5a34e67c
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-05-27 18:08:32 +0500

    Changes applied bugs fixed

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
-from authentication.models import User
 
 
 class Genre(models.Model):
@@ -45,13 +45,13 @@ class Movie(models.Model):
 
 
 class Saved(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # write after authentication
+    # user = models.ForeignKey(MyUser, on_delete=models.CASCADE)  # write after authentication
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) # write after authentication
+    # user = models.ForeignKey(MyUser, on_delete=models.CASCADE) # write after authentication
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.TextField()  # message
     rating = models.FloatField()
