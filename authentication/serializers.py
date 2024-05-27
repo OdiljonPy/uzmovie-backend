cc8a3a5c15e085442d25c072e75e745183152da0
bilol <bilolnorov7@gmail.com>
2024-05-27 18:22:25 +0500

    Authentication: otp-request-response serializers are added tokenobtainserializer overwrited

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,11 @@
-from rest_framework.serializers import ModelSerializer
-from authentication.models import User
+from typing import Dict, Any
+
+from rest_framework.serializers import ModelSerializer, Serializer
+from .models import User, OTPRegisterResend
+from rest_framework import serializers
+from .utils import username_validation
+from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from django.core.exceptions import ValidationError
 
 
 class UserSerializer(ModelSerializer):
@@ -8,3 +14,25 @@ class UserSerializer(ModelSerializer):
         fields = ('username', 'first_name', 'last_name', 'profile_picture', 'is_verified', 'is_active')
 
 
+class UserRequestSerializer(Serializer):
+    username = serializers.CharField(max_length=12, validators=[username_validation])
+    password = serializers.CharField(max_length=50)
+
+
+class LoginSerializer(TokenObtainSerializer):
+    def validate(self, attrs):
+        if self.user.is_verified:
+            data = super().validate(attrs)
+            return data
+        else:
+            raise ValidationError("User is not verified")
+
+
+class OTPRegisterResendSerializer(ModelSerializer):
+    class Meta:
+        model = OTPRegisterResend
+        fields = ('otp_key', )
+
+
+class OTPRegisterResendRequestSerializer(Serializer):
+    otp_code = serializers.IntegerField()
