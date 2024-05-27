824c6dec1c1a7213a56d654c535e07ee155d580e
bilol <bilolnorov7@gmail.com>
2024-05-27 16:07:01 +0500

    Authentication: some bugs and errors have been fixes

--- authentication/models.py
+++ authentication/models.py
@@ -1,33 +1,36 @@
 import uuid
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from .utils import username_validation, generate_otp_code
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50, blank=True)
-    is_verified = models.BooleanField(default=False)
+    username = models.CharField(
+        max_length=12,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validation],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
 
-    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    is_verified = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
 
     def __str__(self):
         return self.username
 
 
-
-class OTP_code_save(models.Model):
-    otp = models.IntegerField(unique=True)
-    username = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
+class OTPRegisterResend(models.Model):
     otp_key = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    otp_code = models.PositiveIntegerField(default=generate_otp_code)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return str(self.created_at)
 
 
