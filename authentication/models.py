579a04db6a50544aabc2814c970b91f583c336d5
ozodbek <ozodbekkkk13@icloud.com>
2024-05-27 14:54:43 +0500

    class User: base-AbstractUser is_verified_default = False, //will be changed to True only if the user enters the OTP in the  given time
    class OTP_code_save:
    save otp code with username when the code is sent to telegram

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,33 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class User(AbstractUser):
-    is_verified = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50, blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+
+class OTP_code_save(models.Model):
+    otp = models.IntegerField(unique=True)
+    username = models.ForeignKey(User.username, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    otp_key = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
 
 
