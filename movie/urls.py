d40b9c7128c561e00570a4cfc7ffe7720fe5ae33
asus <sahruz708@gmail.com>
2024-05-29 20:49:54 +0500

    added comment urls and search urls

--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import MovieViewSet, SavedViewSet
+from .views import MovieViewSet, SavedViewSet, CommentViewSet, SearchAPIView
 
 urlpatterns = [
+    # SEARCH
+    path('search/', SearchAPIView.as_view()),
     # MOVIE
     path('get-all/', MovieViewSet.as_view({'get': 'get_all'})),
     path('by-id/', MovieViewSet.as_view({'get': 'get_by_id'})),
@@ -11,19 +13,9 @@ urlpatterns = [
     # SAVED
     path('save/', SavedViewSet.as_view({'post': 'save_movie'})),
     # COMMENT
-    # path('comment/create/', CommentViewSet.as_view({'post': 'create'})),
-    # path('comment/delete/', CommentViewSet.as_view({'delete': 'delete'})),
-    # MOVIE
-    # path('get-all/', MovieViewSet.as_view({'get': 'get_all'})),
-    # path('get-one/', MovieViewSet.as_view({'get': 'get_one'})),
-    # path('by-genre/', MovieViewSet.as_view({'get': 'get_by_genre'})),
-    # path('by-director/', MovieViewSet.as_view({'get': 'get_by_director'})),
-    # path('by-actor/', MovieViewSet.as_view({'get': 'get_by_actor'})),
-    # # SAVED
-    # path('save/', SavedViewSet.as_view({'post': 'save_movie'})),
-    # # COMMENT
-    # path('comment/create/', CommentViewSet.as_view({'post': 'create'})),
-    # path('comment/delete/', CommentViewSet.as_view({'delete': 'delete'})),
-    # path('comment/update/', CommentViewSet.as_view({'patch': 'update'})),
+    path('comment/create/', CommentViewSet.as_view({'post': 'comment_create'})),
+    path('comment/delete/<int:pk>/', CommentViewSet.as_view({'delete': 'comment_destroy'})),
+    path('comment/review/<int:pk>/', CommentViewSet.as_view({'get': 'comment_review'})),
+    path('comment/list/', CommentViewSet.as_view({'get': 'comment_list'})),
 
 ]
