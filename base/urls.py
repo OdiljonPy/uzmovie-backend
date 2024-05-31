3effcd84fcd4cdfcf24b552baa486e34ca75534f
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-05-31 19:27:13 +0500

    added about model view url and telegram validator

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ContactViewSet
+from .views import ContactViewSet, AboutViewSet
+
 urlpatterns = [
-    path('contact_list/', ContactViewSet.as_view({'get':'contact_list'})),
-    path('contact_create/', ContactViewSet.as_view({'post':'contact_create'}))
+    # contact
+    path('contact/list/', ContactViewSet.as_view({'get': 'contact_list'})),
+    path('contact/create/', ContactViewSet.as_view({'post': 'contact_create'})),
+
+    # about us
+    path('list/', AboutViewSet.as_view({'get': 'about_view'}))
 ]
