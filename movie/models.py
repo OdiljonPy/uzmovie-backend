f1540ff8bf3379981c67bf174df67f0bf35d3b85
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-05-31 09:00:53 +0500

    Subscription field added

--- movie/models.py
+++ movie/models.py
@@ -2,14 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Genre(models.Model):
+class Actor(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Actor(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
