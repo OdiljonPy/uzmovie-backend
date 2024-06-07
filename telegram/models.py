87edf88cc364daef3723b5402b683befdfaa8e93
Asadbek0429 <ravshanbekovasadbek0429@gmail.com>
2024-06-07 19:16:24 +0500

    hot-fix: models changed

--- telegram/models.py
+++ telegram/models.py
@@ -8,8 +8,8 @@ class TelegramUser(models.Model):
     balance = models.IntegerField(default=0)
     is_subscribed = models.BooleanField(default=False)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return str(self.user_id)
@@ -19,8 +19,8 @@ class Saved(models.Model):
     user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='telegram_saved_movie')
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.movie.title
