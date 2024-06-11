054182b8eecae79ecc543118c4918956a86fc5cc
Dilnavoz-tech <nabiyevadilnavoz736@gmail.com>
2024-06-11 23:35:58 +0500

    Models: Added deleted_at fields to Choice Model and Card Models

--- payments/models.py
+++ payments/models.py
@@ -16,6 +16,7 @@ class Choice(models.Model):
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField(default=0)
     description = models.TextField(max_length=260, blank=True, null=True)
+    deleted_at = models.DateField(null=True)
 
     def __str__(self):
         return self.name
@@ -58,6 +59,7 @@ class Card(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateField(null=True)
 
     def __str__(self):
         return self.holder_full_name
