481eb4fe3fec9feafdbbac25077efac7b47f3b3a
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-06-15 10:37:29 +0500

    contact: a changed

--- base/views.py
+++ base/views.py
@@ -68,7 +68,7 @@ class AboutViewSet(ViewSet):
         tags=['contact']
     )
     def about(self, request):
-        if not request.user.is_authenticated:
+        if request.user.is_authenticated:
             return Response(
                 data={'error': 'Not authenticated'},
                 status=status.HTTP_401_UNAUTHORIZED
